1b417d67c0c86021cee696a3743bbb0ed444a7fc
Manoel Lima <manoel.lima@manaus.am.gov.br>
2025-03-31 17:05:20 -0400

    feat: add card to show product

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,11 +1,13 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/products.interface';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatCardModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
